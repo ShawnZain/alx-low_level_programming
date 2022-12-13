c01d957466075e4d18275853ab1b87f6eb83f3db
ShawnZain <shawnzainpatel@gmail.com>
2022-12-12 19:24:59 -0800

    Completed task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabets expect 'q' & 'e'
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if ((alphabet != 'e') && (alphabet != 'q'))
+		{
+			putchar(alphabet);
+		}
+		alphabet++;
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
