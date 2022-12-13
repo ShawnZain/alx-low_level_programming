43977242c43dd3ad20fdfda72d3957423efb0308
ShawnZain <shawnzainpatel@gmail.com>
2022-12-12 18:03:13 -0800

    Completed task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,23 +3,24 @@
 #include <time.h>
 
 /**
- * main - Determins if random num is +//-
- * 
- * Return: Always 0 (successful)
- * 
+ * main - main function to check if random num is +//-
+ *
+ * Return: Always 0 (success)
+ *
  */
 
 int main(void)
 {
-	
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n == 0)
+	else if (n ==0)
 	{
 		printf("%d is zero\n", n);
 	}
