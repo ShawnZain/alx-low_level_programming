5aa7e300e239706d3d60cc11a90ed9e9cd452391
ShawnZain <shawnzainpatel@gmail.com>
2022-12-13 08:36:54 -0800

    Completed task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints combinations of single-digit numbers in ascending order
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
