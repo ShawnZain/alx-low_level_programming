8ae6d6a87ec505f0d25c30b764bdc07480f45ea2
ShawnZain <shawnzainpatel@gmail.com>
2022-12-13 08:42:27 -0800

    Completed task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,18 +12,19 @@ int main(void)
 {
 	int num = '0';
 
-	while (num <= '9')
+	while (num <= '9') /*Print all numbers from 1 to 9*/
 	{
 		putchar(num);
 
-		if (num != '9')
+		if (num != '9') /*Making sure that ", " does not appear at the end after 9, as that is the last number*/
 		{
-			putchar(',');
+			putchar (',');
 			putchar(' ');
 		}
+
 		num++;
 	}
-	
+
 	putchar('\n');
 
 	return (0);
