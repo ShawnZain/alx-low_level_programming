3f8d1856b9450228ecc5723aabe5afdf8dd4d868
ShawnZain <shawnzainpatel@gmail.com>
2022-12-12 19:43:20 -0800

    Completed task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabets in reverse
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+
+	return (0);
+}
