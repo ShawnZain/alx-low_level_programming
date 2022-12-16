6b03b72c5d20db91be0c2fb519b406a21d21857e
ShawnZain <shawnzainpatel@gmail.com>
2022-12-16 10:31:17 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lowecase
+ *
+ * Return: ALways 0 (success)
+ *
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+
+}
