b77a217372d1784520cbb875778bcc22f0fa18f2
ShawnZain <shawnzainpatel@gmail.com>
2022-12-16 12:43:20 -0800

    Advanced task 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,9 +18,9 @@ void print_times_table(int n)
 	if (n < 0 || n >= 15)
 		return;
 
-	for (row = 0; row <== n; row++)
+	for (row = 0; row <= n; row++)
 	{
-		for (colum = 0; column <= n; column++)
+		for (column = 0; column <= n; column++)
 		{
 			product = (row * column);
 
@@ -41,7 +41,7 @@ void print_times_table(int n)
 				else if (product > 9 && product < 100)
 				{
 					_putchar(' ');
-					_puthcar('0' + (product / 10));
+					_putchar('0' + (product / 10));
 					_putchar('0' + (product % 10));
 				}
 				else if (product >= 100)
