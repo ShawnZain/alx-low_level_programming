0978e82724b075548947ec3fc8260532db97c0eb
ShawnZain <shawnzainpatel@gmail.com>
2022-12-16 13:20:28 -0800

    Advanced task 3

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints first 50 fibonacci nums
+ *
+ * Returns: Always 0 (success)
+ */
+
+int main(void)
+{
+	long int i, j, k, l;
+
+	j = 1;
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+			printf("%ld, ", j);
+		else
+			printf("%ld\n", j);
+
+		l = j + k;
+		j = k;
+		k = l;
+	}
+
+	return (0);
+}
+
