e9e0d903d902025440c280e12a3c4a18c4fabc2d
ShawnZain <shawnzainpatel@gmail.com>
2022-12-18 07:10:02 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies to integers
+ * @a: first integer to multiply
+ * @b: second integer to muliply with
+ * Return: Result of mul
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
