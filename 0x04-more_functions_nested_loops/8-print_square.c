883b96f216e8692c835cfe3bd87915704e49493b
ShawnZain <shawnzainpatel@gmail.com>
2022-12-18 08:05:41 -0800

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,18 +2,19 @@
 
 /**
  * print_square - prints square
- * @n: sqaure dimensions
+ * @size: sqaure dimensions
  */
 
 void print_square(int size)
 {
-	int i = 0, j;
+	int i = 0;
+	int j = 0;
 
 	if (size > 0)
 	{
 		for (; i < size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (; j < size; j++)
 			{
 				_putchar(35);
 			}
@@ -21,5 +22,7 @@ void print_square(int size)
 		}
 	}
 	else
+	{
 		_putchar('\n');
+	}
 }
