7b27d711b0f466c940d692ea8034d66aad0becde
ShawnZain <shawnzainpatel@gmail.com>
2022-12-19 10:33:17 -0800

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - prints the length of a string
+ * @s: string to be supplied
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
