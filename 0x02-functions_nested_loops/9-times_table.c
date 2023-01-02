811372c95c4a23389e73d3e44fec50d4eac8b016
ShawnZain <shawnzainpatel@gmail.com>
2023-01-02 07:23:34 -0800

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,9 +2,6 @@
 
 /**
  * times_table - prints the 9 times table
- *
- * Return: Always 0 (success)
- *
  */
 
 void times_table(void)
@@ -17,39 +14,28 @@ void times_table(void)
 		{
 			c = a * b;
 
-			if ((c / 10) == 0)
+			if (b == 0)
+				_putchar('0');
+
+			else if (b != 0)
 			{
-				if (b == 0)
+				if (c / 10 == 0)
 				{
-					_putchar('0');
-				}
-
-				if (b != 0)
-				{
-					_putchar (' ');
-					_putchar((c % 10) + '0');
-				}
-
-				if (b < 9)
-				{
-					_putchar(',');
 					_putchar(' ');
+					_putchar(c % 10 + '0');
+				}
+				else
+				{
+					_putchar(c / 10 + '0');
+					_putchar(c % 10 + '0');
 				}
 			}
-			else
-			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
 
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
-
-		_putchar('\n');
 	}
 }
-
