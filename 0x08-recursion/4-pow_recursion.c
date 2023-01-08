8e36cce9ec99e46bbf405cb5c6d74125fdf986f3
ShawnZain <shawnzainpatel@gmail.com>
2023-01-08 13:21:31 -0800

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _pow_recursion - calculates the value of x to the power of y.
+ * @x: base
+ * @Y: exponent
+ * Return: the result of x to power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
