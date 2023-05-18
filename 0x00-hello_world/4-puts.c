f523a59b1214e4b8733196872264fb3f656da35d
ShawnZain <shawnzainpatel@gmail.com>
2023-05-18 09:33:53 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * str - What to print out
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return 0;
+}
