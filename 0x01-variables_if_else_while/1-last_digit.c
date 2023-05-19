d8417bc51ad0cca9c8eb0fb85f5f3b547fcabba0
ShawnZain <shawnzainpatel@gmail.com>
2023-05-19 12:31:03 -0700

    Using if and else if with logical and relational operators. Also did arithmetic with printf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,17 +20,17 @@ int main(void)
 
 	if (n % 10 > 5)
 	{
-		printf("Last digit of %d is %d and is greater 
-				than 5\n", n, n % 10);
+		printf("Last digit of %d is %d", n, n % 10);
+		printf(" and is greater than 5\n";
 	}
 	else if (n % 10 == 0)
 	{
-		printf("Last digit of %d is 0 and 0", n);
+		printf("Last digit of %d is 0 and is 0\n", n);
 	}
 	else if (n % 10 < 6 && n % 10 != 0)
 	{
-		priintf("Last digit of %d is %d and is less 
-				than 6 and not 0", n, n % 10);
+		priintf("Last digit of %d is %d", n, n % 10);
+		printf(" and is less than 6 and not 0\n");
 	}
 
 	return (0);
