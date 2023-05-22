cd6c4762ac6eb13e993f9325fbd5eb11745c3481
ShawnZain <shawnzainpatel@gmail.com>
2023-05-22 12:18:42 -0700

    Tricky question, think outside the box

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,61 +3,44 @@
 /**
  * main - entry point
  *
- * Description: print combination of 2 2-digit numbers
- * the 2 numbers should be separated by a space
- * the combinations should be separated by ', '
- * the same 2-digits should not be repeated in 2 different combinations
- * 01 00 and 00 01 are the same combination of 01 and 00
- * only use putchar 8 times
- * don't use variable char
+ * Description: prints possible combos of two 2-digit numbers
+ * 99 99 should not be printed
+ * 01 00 and 00 01 should not be printed
+ * only use putchar
+ * end in a newline
+ *
+ * Return: Success (0)
  */
 
 int main(void)
 {
-	int num1, num2, num3, num4;
-	int result1, result2;
+	int num1, num2;
 
-	/* loop to run from 0 to 9 for 4 numbers */
-
-	for (num1 = 0; num1 <= 9; num1++)
+	for (num1 = 0; num1 <= 98; num1++) /* num1 will run from 0 to 98 */
 	{
-		for (num2 = 0; num2 <= 9; num2++)
+		for ((num2 = num1 + 1); num2 <= 99; num2++)
 		{
-			for (num3 = 0; num3 <= 9; num3++)
-			{
-				for (num4 = 0; num4 <= 9; num4++)
-				{
-					/* two numbers should not be the same
-					 * 00 10 & 10 00 is a repeat combo */
+			/**
+			 * num2 runs from 01 to 99 
+			 * num1 will never be equal to num2
+			 */
 
-					result1 = num1 * 10 + num2;
-					result2 = num3 * 10 + num4;
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-					if (result1 != result2)
-					{
-						putchar(num1 + '0');
-						putchar(num2 + '0');
-						putchar(' ');
-						putchar(num3 + '0');
-						putchar(num4 + '0');
+			/**
+			 * if num1 gets to the max & num2 gets to the max
+			 * ignore the next bits of code
+			 */
 
-						/* at the end of the sequence
-						 * don't print ', ' */
+			if (num1 == 98 && num2 == 99)
+				continue;
 
-						if ((result1 != 98) &&
-						(result2 != 99))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-						else if ((result1 == 98) &&
-							(result2 == 99))
-						{
-							break;
-						}
-					}
-				}
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
