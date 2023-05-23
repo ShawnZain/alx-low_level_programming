bdaf0eb3a834018eefbd9f2b5aa77e8ebe6b7f52
ShawnZain <shawnzainpatel@gmail.com>
2023-05-23 12:39:05 -0700

    Absolute integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <math.h>
+
+/**
+ * _abs - gives the absolute value of integers
+ * @n: argument to test
+ * Return: the absolute integer
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else if (n < 0)
+	{
+		int absolute;
+		absolute = n * -1;
+		return (absolute);
+	}
+	return (0);
+}
+
