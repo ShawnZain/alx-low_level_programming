f815cf1a374d12d4e69ff1a5c94f07533dbf84fc
ShawnZain <shawnzainpatel@gmail.com>
2023-05-23 11:22:01 -0700

    functions with nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabets followed by a newline
+ * ten times
+ * @void - no parameters
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+
+		_putchar('\n');
+
+	}
+}
