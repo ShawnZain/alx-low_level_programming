c56ef4a9388c8743453abef5ace5aa534a56cb73
ShawnZain <shawnzainpatel@gmail.com>
2023-05-25 11:31:49 -0700

    Print 0 to 9 but not 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 to 9 except 2 and 4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= 9)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
