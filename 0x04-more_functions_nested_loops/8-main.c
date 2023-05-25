1f54a8497afb995943a791df49961ddc1f1608d6
ShawnZain <shawnzainpatel@gmail.com>
2023-05-25 14:42:29 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - checks code
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_square(2);
+	print_square(6);
+	print_square(-5);
+	print_square(0);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: length and width of the square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int height, width;
+
+	height = 1;
+
+	if (size > 0)
+	{
+		while (height <= size)
+		{
+			width = 0;
+			while (width < size)
+			{
+				_putchar(35);
+				width++;
+			}
+			_putchar('\n');
+			height++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
