7df73bb3d98fdf51c69a810a74f02eab0e5fd4e3
ShawnZain <shawnzainpatel@gmail.com>
2023-05-30 16:11:34 -0700

    Copy a string to another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+/**
+ * _strcpy - copies one string into another
+ * @dest: paste the string here
+ * @src: copy this string
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	/* find the length of the string we will copy */
+	while (*(src + i) =! '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		dest[j] = src[j];
+	}
+
+	/* add the terminating null byte */
+	dest[j] = '\0';
+
+	return (dest);
