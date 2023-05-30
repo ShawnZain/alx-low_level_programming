a2f195c2dc36e267a0f5b4dad6355399197e976a
ShawnZain <shawnzainpatel@gmail.com>
2023-05-30 12:49:40 -0700

    Function to get the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strleng - finds the length of a string
+ * @s - string whose length to check for
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i, l;
+
+	for (i = 1; s[i-1] != '\0'; i++)
+	{
+		l = i;
+	}
+
+	return (l + 1);
+}
