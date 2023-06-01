fe32ad126ffaf50c8e09dc7a7c4006d20f52cf88
ShawnZain <shawnzainpatel@gmail.com>
2023-06-01 06:41:25 -0700

    Concatenate 2 strigns

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,46 @@
+/**
+ * _strcat - concatenates two strings
+ * @dest: the string to add to
+ * @src: the string to add to dest
+ *
+ * pseudo code - 
+ * find the lengths of both strings until their null byte
+ * then from dest's null byte rewrite with values of src
+ * thereafter add the null byte to dest to end the string
+ *
+ * Return: pointer to the concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	int len;
+
+	/* find length of dest until the null byte */
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	/* find the length of src until null byte */
+	j = 0;
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+
+	/* from null byte of dest, replace the value of dest with src */
+	len = 0;
+	while (len < j) /* src[len] will go until just before '\0' of src */
+	{
+		dest[i] = src[len]; 
+		len++;
+		i++; /* increment i so that the strings concatenate */
+	}
+
+	/* after all values of src are concatenated in dest add '\0' */
+	dest[i] = '\0';
+
+	return (dest);
+}
