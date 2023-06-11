245effab64fbac8a98d8b52a6c1261705e3c3f27
ShawnZain <shawnzainpatel@gmail.com>
2023-06-11 06:52:46 -0700

    Static Libraries :(

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,1 +1,26 @@
-char *_strpbrk(char *s, char *accept);
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+			return (s);
+		}
+
+		s++;
+	}
+
+
+	return ('\0');
+}
