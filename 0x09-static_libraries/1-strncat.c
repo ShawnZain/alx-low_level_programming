168d5b5f2d88ce076cdb4ab7d1ee4a654eaaf178
ShawnZain <shawnzainpatel@gmail.com>
2023-06-11 06:31:35 -0700

    Static Libraries :(

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,10 +1,33 @@
 /**
- * _strncat - concatenates 2 string using only n bytesfrom src
- * @dest: string one
- * @src: string two to concatenate to string 1
- * @n: number of bytes from src to concatenate to dest
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
- * Return: pointer to the concatenated string
+ * Return: dest
  */
 
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
