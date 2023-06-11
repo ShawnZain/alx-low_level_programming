dd9d8a996b7f2380f7f777fd7e8c45c13d22ae97
ShawnZain <shawnzainpatel@gmail.com>
2023-06-11 06:25:12 -0700

    Static Libraries :(

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,7 +1,10 @@
 /**
- * _isdigit - checks if input provided is a digit
- * @c: int to check
- * Return: 1 if c is a digit, 0 otehrwise
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be checked
+ * Return: 1 if c is a digit, 0 otherwise
  */
 
-int _isdigit(int c);
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
