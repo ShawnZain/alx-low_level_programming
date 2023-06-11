4c0ddda3fd35efa24dfabd584b0735b67ce3fdaf
ShawnZain <shawnzainpatel@gmail.com>
2023-06-11 06:27:50 -0700

    Static Libraries :(

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,9 +1,22 @@
 /**
- * _memcpy - copies bytes from one string to another
- * @dest: where to copy
- * @src: what to copy
- * @n: bytesof src to copy to dest
- * Return: changed string
+ * _memcpy - a function that copies memory area
+ * @dest: memory where is stored
+ * @src: memory where is copied
+ * @n: number of bytes
+ *
+ * Return: copied memory with n byted changed
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
