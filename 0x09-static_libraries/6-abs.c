74cf481b1d1f739a374526d2cffa3e515895154a
ShawnZain <shawnzainpatel@gmail.com>
2023-06-11 06:54:24 -0700

    Static Libraries :(

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,1 +1,14 @@
-int _abs(int n);
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
