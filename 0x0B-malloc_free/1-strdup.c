d407ccf78dfe36d2edb3e09a9365ac93a4717dd6
ShawnZain <shawnzainpatel@gmail.com>
2023-06-13 13:34:27 -0700

    Duplicate a string to a specific size in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string provided to another location in memory
+ * @str: string to duplicate
+ * Return: NULL if str is NULL
+ * NULL if malloc fails
+ * pointer to the duplicated string on success
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	int a, b;
+
+	if (str == NULL)
+		return (NULL);
+
+	a = 0;
+	while (str[a] != '\0');
+	{
+		a++;
+	}
+
+	dup = malloc(sizeof(char) * a);
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (b = 0; str[b]; b++)
+	{
+		dup[b] = str[b];
+	}
+
+	return (dup);
+}
