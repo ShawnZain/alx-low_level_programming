704a8948e094c16cb2004b49af851f983b487417
ShawnZain <shawnzainpatel@gmail.com>
2023-06-13 14:39:31 -0700

    creating a grid of specified height and width

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,50 +1,43 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - create a grid with 0s
- * @width: width of the grid
- * @height: height of the grid
- * Return: NULL if height of width are <= 0
- * NULL if malloc fails
- * pointer to the 2 dimensional array
- */
+* alloc_grid - nested loop to make grid
+* @width: width input
+* @height: height input
+* Return: pointer to 2 dim. array
+*/
 
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	int a , b;
+	int **mee;
+	int x, y;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid[a][b] = malloc(sizeof(int) * height * width);
-	if (grid == NULL)
-	{
-		free(grid[a][b]);
+	mee = malloc(sizeof(int *) * height);
+
+	if (mee == NULL)
 		return (NULL);
+
+	for (x = 0; x < height; x++)
+	{
+		mee[x] = malloc(sizeof(int) * width);
+
+		if (mee[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(mee[x]);
+		free(mee);
+		return (NULL);
+		}
 	}
 
-/**
- * for (a = 0; a < height; a++)
- * {
- * 	grid[a] = malloc(sizeof(int) * width);
- * 	if (grid[a] == NULL)
- * 	{
- * 		for (; a >= 0; a--)
- * 			free(grid[a]);
- * 		free(grid);
- * 		return (NULL);
- * 		}
- * }
- */
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+			mee[x][y] = 0;
+	}
 
-		for (a = 0; a < height; a++)
-		{
-			for (b = 0; b < width; b++)
-			{
-				grid[a][b] = 0;
-			}
-		}
-
-	return (grid);
+	return (mee);
 }
