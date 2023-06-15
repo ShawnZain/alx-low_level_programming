7290ecda9e4f50efce6a412719fae468e929155d
ShawnZain <shawnzainpatel@gmail.com>
2023-06-15 13:42:44 -0700

    Allocating memory and checking if malloc returns NULL

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * if malloc fails, exit (98)
+ *
+ * @b: size of memory to allocate
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
