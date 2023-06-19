02d1e1f7a5cb3ad542e48aafc6928335275ef1e8
ShawnZain <shawnzainpatel@gmail.com>
2023-06-19 12:19:18 -0700

    Creating an array of type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - creates a struct of trype struct dog
+ * @name: member1
+ * @age: member2
+ * @owner: member3
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
