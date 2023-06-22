638155fc4008f8630dcc18645cc5394ad56219ed
ShawnZain <shawnzainpatel@gmail.com>
2023-06-22 12:40:22 -0700

    Using function pointers to print out the name provided

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -46,6 +46,7 @@ void print_name_uppercase(char *name)
 int main(void)
 {
     print_name("Bob", print_name_as_is);
+    print_name_as_is("Bob");
     print_name("Bob Dylan", print_name_uppercase);
     printf("\n");
     return (0);
--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,14 @@
 /**
  * print_name - prints the name
  * @name: name to print
- * @f(): function pointer with the print function
+ * @f: function pointer with the print function
  * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	/* DEBUGGING: making sure that valid arguments are passed to the funciton */
+	if (!name || !f)
+		exit(1);
 	f(name);
 }
