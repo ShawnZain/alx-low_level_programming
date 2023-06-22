fe696e13acd6a9807e4b8bb68410f67600489ad5
ShawnZain <shawnzainpatel@gmail.com>
2023-06-22 13:12:50 -0700

    Function that iterates through a given array and executes a function on each element of the array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include <stdlib.h> /* exit */
+
+/**
+ * array_iterator - a function that executes a function on each
+ * element of an array
+ *
+ * @array: array to iterate through
+ * @size: size of the array
+ * @action: function pointer pointing to the function that will
+ * be executed on each element of the array
+ *
+ * Return: nothing
+ */
+
+void array_iterator(char *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+		exit(1);
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,6 @@
 #define HEADER_H
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(char *array, size_t size, void (*action)(int));
 
 #endif /* HEADER_H */
