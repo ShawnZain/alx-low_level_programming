1b1e82471b6400e94bceeedb31fba4fcb9db6ae2
ShawnZain <shawnzainpatel@gmail.com>
2023-07-04 11:54:29 -0700

    A function that counts how many nodes there are in the list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - counts how many nodes there are in a list
+ * h: the head of the list
+ * Return: the number of elements (nodes) in the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	list_t *temp = h;
+	int counter = 0;
+
+	while (temp != NULL)
+	{
+		counter++;
+		temp = temp->next;
+	}
+
+	return (counter);
+}
