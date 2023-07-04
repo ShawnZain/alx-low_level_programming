8629ed707610636fd6a294848a0c8c7972195c6f
ShawnZain <shawnzainpatel@gmail.com>
2023-07-04 14:37:03 -0700

    A function that frees all dynamically allocated memory of the list and sets the head pointer to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_linstint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the dynamically allocated list items in memory
+ * and sets the head pointer to NULL
+ *
+ * head: the pointer to the head node
+ *
+ * Return: nothing
+ */
+
+void free_linstint2(listint_t **head)
+{
+	listint_t *temp = *head;
+	listint_t *node;
+
+	if (head != NULL)
+	{
+		while (temp != NULL)
+		{
+			node = temp;
+			temp = temp->next;
+			free(node);
+		}
+		*head = NULL;
+	}
+}
