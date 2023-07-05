3ac6a4370d00fa83c815f10c721ad531334bb205
ShawnZain <shawnzainpatel@gmail.com>
2023-07-05 12:47:57 -0700

    A function that searches through the list to return a pointer to the node at the index provided

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - looks for the nth node of a list
+ * @head: the pointer to the head of the linked list
+ * @index: the index to search for in the list
+ * Return: the pointer to the nth node
+ * else if the node does not exist, return NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp = head;
+	unsigned int counter = 0;
+
+	while (temp != NULL)
+	{
+		if (counter == index)
+		{
+			return (temp);
+		}
+		else
+		{
+			temp = temp->next;
+		}
+		counter++;
+	}
+
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+}
+
