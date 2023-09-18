250d8acfb64b37d08ef0e60d9afc66ae47d5acac
ShawnZain <shawnzainpatel@gmail.com>
2023-09-18 13:49:33 -0700

    Task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,33 +10,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, i = 0;
+	int o, w, len = 0;
+
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i];)
-		{
-			i++;
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
+	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | 0600);
-	if (fd < 0)
-	{
+
+	if (o == -1 || w == -1)
 		return (-1);
-	}
 
-	w = write(fd, text_content, i);
-	if (w < 0)
-	{
-		write(1, "Cannot write to file", 20);
-		return (-1);
-	}
 
-	close(fd);
+	close(o);
 
 	return (1);
 }
